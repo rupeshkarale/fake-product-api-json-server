74f644799763f0877a8e7ee67c1ca3016d1b8db3
Rupesh Karale <rupeshkarale3245@gmail.com>
2022-09-06 14:38:30 +0530

    push

--- db.js
+++ db.js
@@ -1,36 +1,10 @@
-const items = require("./data/items");
-const tags = require("./data/tags");
-const brands = require("./data/brands");
-const itemTypes = require("./data/itemTypes");
+const items = require("./data/team");
+
 
 module.exports = {
-  items,
-  tags,
-  brands,
-  itemTypes
+  items
+ 
 }
 
 
-// const items = require("./data/items");
 
-// module.exports = () => {
-
-//   var tags = items.map((i) => i.tags);
-//   tags = [].concat.apply([], tags);
-//   tags = [...new Set(tags)];
-
- 
-
-//   var brands = items.map((i) => i.manufacturer);
-//   brands = [...new Set(brands)];
-
-//   var itemTypes = items.map((i) => i.itemType);
-//   itemTypes = [...new Set(itemTypes)];
-
-//   return {
-//     items,
-//     tags,
-//     brands,
-//     itemTypes,
-//   };
-// };
