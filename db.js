a8f90ce819a9f74c479e8334e04b97c5f7733284
Rupesh Karale <rupeshkarale3245@gmail.com>
2022-09-09 13:22:37 +0530

    push

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
 const items = require("./data/team");
 const match = require("./data/match");
 const player = require("./data/player");
+const login = require("./data/login");
 
 
 
 module.exports = {
   items,
   match,
-  player
+  player,
+  login
  
 }
 
